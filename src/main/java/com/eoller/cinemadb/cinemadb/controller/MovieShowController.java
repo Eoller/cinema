b2cd57c627efed45f21fe40baa4fb0013ddf299a
eoller <Egorpyp@gmail.com>
2019-06-17 18:41:08 +0200

    general motors

--- src/main/java/com/eoller/cinemadb/cinemadb/controller/MovieShowController.java
+++ src/main/java/com/eoller/cinemadb/cinemadb/controller/MovieShowController.java
@@ -1,17 +1,17 @@
 package com.eoller.cinemadb.cinemadb.controller;
 
+import com.eoller.cinemadb.cinemadb.domain.Movie;
 import com.eoller.cinemadb.cinemadb.domain.MovieShow;
 import com.eoller.cinemadb.cinemadb.domain.Reservation;
+import com.eoller.cinemadb.cinemadb.domain.dto.MovieMovieShowDto;
+import com.eoller.cinemadb.cinemadb.repository.MovieRepository;
 import com.eoller.cinemadb.cinemadb.repository.MovieShowRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 @RestController
@@ -19,6 +19,9 @@ public class MovieShowController {
 
     @Autowired
     private MovieShowRepository movieShowRepository;
+
+    @Autowired
+    private MovieRepository movieRepository;
 
     @GetMapping("/movieshow")
     public ResponseEntity getAllByMovieId(@RequestParam long movieId){
@@ -42,7 +45,14 @@ public class MovieShowController {
             return new ResponseEntity(showsByCinemaId, HttpStatus.OK);
         }
         List<MovieShow> collect = showsByCinemaId.stream().filter(movieShow -> movieShow.getMovie().getId() == movieId).collect(Collectors.toList());
-        return new ResponseEntity(collect, HttpStatus.OK);
+        Set<Movie> movies = collect.stream().map(MovieShow::getMovie).collect(Collectors.toSet());
+        List<MovieMovieShowDto> movieMovieShows = new ArrayList<>();
+        movies.forEach(movie -> {
+            List<MovieShow> movieShowList = collect.stream().filter(movie1 -> movie1.getMovie().getId() == movie.getId()).collect(Collectors.toList());
+            MovieMovieShowDto dto = new MovieMovieShowDto(movie, movieShowList);
+            movieMovieShows.add(dto);
+        });
+        return new ResponseEntity(movieMovieShows, HttpStatus.OK);
     }
 
 
